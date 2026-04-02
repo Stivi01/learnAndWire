66c459e31d4c9d5d045c9f98245bed774e3c0908
Stivi01 <stefan.nitajr@gmail.com>
2026-04-02 19:43:13 +0300

    Add functionality for teacher-profile.

--- frontend/src/app/teacher/teacher-profile/teacher-profile.ts
+++ frontend/src/app/teacher/teacher-profile/teacher-profile.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { StudentProfileData, User } from '../../core/services/user';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-teacher-profile',
-  imports: [],
+  imports: [CommonModule, FormsModule],
+  standalone: true,
   templateUrl: './teacher-profile.html',
   styleUrl: './teacher-profile.scss',
 })
 export class TeacherProfile {
+  private userService = inject(User);
+  private authService = inject(AuthService);
 
+  profile = signal<StudentProfileData>({
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    academicYear: 0,
+    avatar: ''
+  });
+
+  isLoading = signal(true);
+  successMessage = signal('');
+  errorMessage = signal('');
+
+  constructor() {
+    this.loadProfile();
+  }
+
+  loadProfile() {
+    this.userService.getProfile().subscribe({
+      next: (data) => {
+        console.log("Avatar value from server:", data.avatar);
+        this.profile.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.errorMessage.set('Eroare la încărcarea profilului.');
+        this.isLoading.set(false);
+      }
+    });
+  }
+
+  uploadAvatar(event: any) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('avatar', file);
+
+    this.userService.uploadAvatar(formData).subscribe({
+      next: (res) => {
+        this.profile.set({ ...this.profile(), avatar: res.avatar });
+        this.authService.updateUserAvatar(res.avatar || '');
+        this.successMessage.set('Avatar actualizat!');
+      },
+      error: () => {
+        this.errorMessage.set('Eroare la încărcarea avatarului.');
+      }
+    });
+  }
+
+  saveProfile() {
+    this.userService.updateProfile(this.profile()).subscribe({
+      next: () => {
+        this.successMessage.set('Profilul a fost actualizat cu succes!');
+      },
+      error: () => {
+        this.errorMessage.set('Eroare la actualizarea profilului.');
+      }
+    });
+  }
 }
